10130c53aafe19c2c46682ea007abcdf3b9338a0
rhexa <akun.kripitit@gmail.com>
2021-10-10 22:00:26 +0300

    added update user and get user by jwt

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const verify = require("../verifyToken");
+const jwt = require("jsonwebtoken");
 const User = require('../model/user');
 
 // get all users
@@ -19,16 +20,61 @@ router.get('/', verify, async (req, res) => {
     }
 })
 
-// get user profile by id
-router.get('/:id', verify, async (req, res) => {
+// update user
+router.put('/', verify, async (req, res) => {
+    const token = req.header('auth-token')
+    const decode = await jwt.decode(token)
+    const id = decode._id
+    const updatedUser = {
+        email: req.body.email,
+        city: req.body.city,
+        fav_food: req.body.fav_food
+    }
     try {
-        const user = await User.findById(req.params.id);
+        const user = await User.findByIdAndUpdate(id, updatedUser, {new:true})
         res.json({
             success: 1,
             user
         })
     } catch (error) {
-        console.log(err);
+        console.log(error);
+        res.status(500).json({
+        success: 0,
+        message: "Something went wrong",
+        });
+    }
+})
+
+// get user profile by id
+// router.get('/:id', verify, async (req, res) => {
+//     try {
+//         const user = await User.findById(req.params.id);
+//         res.json({
+//             success: 1,
+//             user
+//         })
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({
+//         success: 0,
+//         message: "Something went wrong",
+//         });
+//     }
+// })
+
+// get my profile
+router.get('/me', verify, async (req, res) => {
+    const token = req.header('auth-token')
+    const decode = await jwt.decode(token)
+    const id = decode._id
+    try {
+        const user = await User.findById(id);
+        res.json({
+            success: 1,
+            user
+        })
+    } catch (error) {
+        console.log(error);
         res.status(500).json({
         success: 0,
         message: "Something went wrong",
